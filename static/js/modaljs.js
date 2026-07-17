f458a55ed4d46e8cb840e6be9986388da8f3701e
renegadelha <renegadelha@gmail.com>
2026-07-17 16:19:11 -0300

    aula 1707

---
+++ static/js/modaljs.js
@@ -0,0 +1,12 @@
+
+const modal = document.getElementById('meuModal');
+const btnAbrir = document.getElementById('btnAbrir');
+const btnFechar = document.getElementById('btnFechar');
+
+function abrirmodal(){
+    modal.style.display = 'block';
+}
+
+function fechar(){
+    modal.style.display = 'none';
+}
