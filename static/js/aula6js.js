6e495f7624fa8ecfb8708cffbc445efecfe4987a
renegadelha <renegadelha@gmail.com>
2026-07-17 15:55:16 -0300

    aula 1707

--- static/js/aula6js.js
+++ static/js/aula6js.js
@@ -1,4 +1,9 @@
 function apagar(){
+    if(document.body.style.backgroundColor == "black"){
+        document.body.style.backgroundColor = "white";
+            document.body.style.color = "black";
+    }else{
             document.body.style.backgroundColor = "black";
             document.body.style.color = "white";
-        }
+    }
+}
