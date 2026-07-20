b53df4fec7abca219c291e75b3abbaefc4ea683c
renegadelha <renegadelha@gmail.com>
2026-07-20 15:33:43 -0300

    aula 2007

--- static/js/modaljs.js
+++ static/js/modaljs.js
@@ -1,7 +1,6 @@
-
+const input_nome = document.getElementById('inputnome');
 const modal = document.getElementById('meuModal');
-const btnAbrir = document.getElementById('btnAbrir');
-const btnFechar = document.getElementById('btnFechar');
+const msg_alerta = document.getElementById('msgalerta');
 
 function abrirmodal(){
     modal.style.display = 'block';
@@ -13,5 +12,13 @@ function fechar(){
 
 function alertardoidao(){
     alert("corre que o homi ta doido");
+}
 
+function mudou(){
+
+    if(input_nome.value === ""){
+        msg_alerta.style.color = "red";
+        msg_alerta.style.fontSize = "8px";
+        msg_alerta.textContent = "Falta você colocar algo";
+    }
 }
